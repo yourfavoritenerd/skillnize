b81be7e98084f8d205ebebdbafe429816693be23
yourfavoritenerd <brunonhamazana@gmail.com>
2024-08-03 14:53:11 +0200

    Coming soon page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,131 @@
+    <!doctype html>
+    <html class="no-js" lang="">
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="x-ua-compatible" content="ie=edge">
+            <title>Skillnize - Modernize Your Skills</title>
+            <meta name="description" content="Modernize Your Skills">
+            <meta name="keywords" content="Skillnize, learn new skills, modernize skills, tutorials, tips, skill development, education">
+            <meta name="author" content="Skillnize">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <!-- Favicon -->
+            <link rel="shortcut icon" type="image/x-icon" href="media/favicon.png">
+            <!-- Bootstrap CSS -->
+            <link rel="stylesheet" href="css/bootstrap.min.css">
+            <!-- Fontawesome CSS -->
+            <link rel="stylesheet" href="css/fontawesome-all.min.css">
+            <!-- Flaticon CSS -->
+            <link rel="stylesheet" href="font/flaticon.css">
+            <!-- Custom Animation CSS -->
+            <link rel="stylesheet" href="css/fxt-animation.css">
+            <!-- Google Web Fonts -->
+            <link href="../../../css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+            <link href="../../../css2-1?family=Quicksand:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+            <!-- Custom CSS -->
+            <link rel="stylesheet" href="style.css">
+        </head>
+        
+    <body>
+        <!--[if lt IE 8]>
+            <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+        <![endif]-->
+
+        <div class="wrapper">
+            <div class="ofcanvas-triger btn-toggle">
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+                <span class="bar"></span>
+            </div>
+            <!-- Main Body Area Start Here -->
+            <div class="fxt-template has-animation">
+                <div class="fxt-bg-element" data-bg-image="media/elements/element1.png"></div>
+                <div class="fxt-main-content">
+                    <div class="fxt-logo">
+                        <a href="#"><img src="media/logo-light.png" alt="Logo"></a>
+                    </div>
+
+                    <div class="fxt-sub-title">HEY GUYS !</div>
+                    <h1 class="fxt-main-title">We're coming soon</h1>
+                    <p class="fxt-paragraph">Modernize your skills!</p>
+                    <div class="fxt-buttons">
+                        <a href="#fxt-page-content" class="btn-toggle fxt-btn fxt-btn-ghost" id="triger-page-content">Mais informações</a>
+                        
+                    </div>
+                    <div class="fxt-socials">
+                        <ul>
+                            <li class="fxt-facebook">
+                                <a href="https://www.youtube.com/@skillnize" target="_blank" title="Facebook"><i class="fab fa-youtube"></i></a>
+                            </li>
+                            <li class="fxt-twitter">
+                                <a href="https://www.facebook.com/skillnize" target="_blank" title="X"><i class="fab fa-facebook-f"></i></a>
+                            </li>
+                            <li class="fxt-google">
+                                <a href="https://www.instagram.com/skillnize" target="_blank" title="google"><i class="fab fa-instagram"></i></a>
+                            </li>
+                            <li class="fxt-linkedin">
+                                <a href="https://www.linkedin.com/company/skillnize/" target="_blank" title="linkedin"><i class="fab fa-linkedin-in"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="fxt-copyright">&copy; 2024 Skillnize. Todos os Direitos Reservados.</div>
+                </div>
+                <div id="fxt-page-content" class="fxt-page-content">
+                    <section class="fxt-about-wrap has-animation">
+                        <div class="container-fluid full-width">
+                            <div class="content-heading">
+                                <div class="translate-bottom delay-500">
+                                    <h2 class="content-title">Sobre Nós</h2>
+                                </div>
+                            </div>
+                            <div class="translate-bottom delay-700">
+                                <p class="content-paragraph">Skillnize é a sua plataforma ideal para aprender novas habilidades e modernizar suas capacidades. Oferecemos tutoriais detalhados, dicas e conteúdo de alta qualidade para impulsionar seu desenvolvimento pessoal e profissional.</p>
+                            </div>
+                        </div>
+                    </section>
+                    <section class="fxt-contact-wrap has-animation">
+                        <div class="container-fluid full-width">
+                            <div class="content-heading">
+                                <div class="translate-bottom delay-300">
+                                    <h2 class="content-title">Contacte-nos</h2>
+                                </div>
+                            </div>
+                            
+                            <div class="fxt-contact-address-wrap">
+                                <div class="translate-bottom delay-1600">
+                                
+                                    <div class="fxt-address-box">
+                                        <div class="item-icon">
+                                            <i class="flaticon-email"></i>
+                                        </div>
+                                        <div class="item-content">
+                                            <h3 class="item-title">Email</h3>
+                                            <div class="item-text">skillnize@skillnize.com</div>
+                                        </div>
+                                    </div>
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+            <!-- Main Body Area End Here -->
+
+        </div>
+        <!-- jquery-->
+        <script src="js/jquery-3.5.0.min.js"></script>
+        <!-- Bootstrap js -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Imagesloaded js -->
+        <script src="js/imagesloaded.pkgd.min.js"></script>
+        <!-- Countdown js -->
+        <script src="js/jquery.countdown.min.js"></script>
+        <!-- Validator js -->
+        <script src="js/validator.min.js"></script>
+        <!-- Custom Js -->
+        <script src="js/main.js"></script>
+
+    </body>
+
+    </html>
